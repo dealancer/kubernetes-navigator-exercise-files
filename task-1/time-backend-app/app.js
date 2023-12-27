651e08b9802cb5697c5a61b618a3fe5bce405433
Vad Myrgorod <vm386@georgetown.edu>
2023-12-27 16:56:48 -0500

    Refactor app code

--- task-1/time-backend-app/app.js
+++ task-1/time-backend-app/app.js
@@ -1,6 +1,8 @@
 const http = require('http');
+
 const hostname = '0.0.0.0';
 const port = 3000;
+
 const server = http.createServer((req, res) => {
   let timestamp = Date.now();
 
@@ -8,6 +10,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end(timestamp + "\n");
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
